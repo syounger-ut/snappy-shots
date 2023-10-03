bd049ca131ffb05d8ce9c739362bd4b8cdcb933a
Sam Younger <syounger@usertesting.com>
2023-10-03 20:35:26 +0100

    Add Comment and Post models, and update ApiController
    * create new endpoints /api/post/:p and /api/post/:p/comments

---
+++ app/models/Comment.scala
@@ -0,0 +1,13 @@
+// app/models/Comment.scala
+
+package models
+
+import play.api.libs.json.{Json, OFormat}
+
+// Represents a comment on a blog post
+case class Comment(id: Int, postId: Int, text: String, authorName: String)
+
+object Comment {
+  // Use a default JSON formatter for the Comment type
+  implicit val format: OFormat[Comment] = Json.format[Comment]
+}
---
+++ app/models/Post.scala
@@ -0,0 +1,15 @@
+// app/models/Post.scala
+
+// Make sure it goes in the models package
+package models
+
+import play.api.libs.json.{Json, OFormat}
+
+// Create our Post type as a standard case class
+case class Post(id: Int, content: String)
+
+object Post {
+  // We're going to be serving this type as JSON, so specify a
+  // default Json formatter for our Post type here
+  implicit val format: OFormat[Post] = Json.format[Post]
+}
