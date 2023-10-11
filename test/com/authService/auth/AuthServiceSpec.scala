d25129dd83c72d9fb6f8fb3bbc37cf20097b2676
Sam Younger <syounger@usertesting.com>
2023-10-11 23:23:33 +0100

    Test AuthService#validate

--- test/com/authService/auth/AuthServiceSpec.scala
+++ test/com/authService/auth/AuthServiceSpec.scala
@@ -3,17 +3,49 @@ package com.authService.auth
 import com.authService.UnitSpec
 import com.authService.mocks.JwtMock
 import pdi.jwt.JwtAlgorithm
+import pdi.jwt.algorithms.JwtHmacAlgorithm
+
+import scala.util.{Failure, Success}
 
 class AuthServiceSpec extends UnitSpec {
+  val mockJwt: JwtMock = mock[JwtMock]
+
   describe("#createToken") {
     it("should should return a token") {
-      val mockJwt = mock[JwtMock]
       (mockJwt.encode (_: String, _: String, _: JwtAlgorithm))
         .expects(*, *, *)
         .returns("fake-jwt-token")
 
       val svs = new AuthService(mockJwt)
       assert(svs.createToken() == "fake-jwt-token")
+    }
+  }
+
+  describe("#validateToken") {
+    describe("when the token is valid") {
+      it("should succeed") {
+        (mockJwt.isValid (_: String, _: String, _: Seq[JwtHmacAlgorithm]))
+          .expects(*, *, *)
+          .returns(true)
+
+        (mockJwt.decodeRawAll (_: String, _: String, _: Seq[JwtHmacAlgorithm]))
+          .expects(*, *, *)
+          .returns(Success("mock-header", "mock-claim", "mock-signature"))
+
+        val svs = new AuthService(mockJwt)
+        assert(svs.validateToken("mock-token") == Success("mock-header", "mock-claim", "mock-signature"))
+      }
+    }
+
+    describe("when the token is not valid") {
+      it("should fail") {
+        (mockJwt.isValid(_: String, _: String, _: Seq[JwtHmacAlgorithm]))
+          .expects(*, *, *)
+          .returns(false)
+
+        val svs = new AuthService(mockJwt)
+        assert(svs.validateToken("mock-token").isFailure)
+      }
     }
   }
 }
--- test/com/authService/mocks/JwtMock.scala
+++ test/com/authService/mocks/JwtMock.scala
@@ -1,11 +1,15 @@
 package com.authService.mocks
 
+import pdi.jwt.algorithms.JwtHmacAlgorithm
 import pdi.jwt.{Jwt, JwtAlgorithm}
 
 import java.time.Clock
+import scala.util.Try
 
 class JwtMock extends Jwt(Clock.systemUTC()) {
-  override def encode(header: String, claim: String, algorithm: JwtAlgorithm): String = {
-    "fake-jwt-token"
-  }
+  override def encode(header: String, claim: String, algorithm: JwtAlgorithm): String = ???
+
+  override def isValid(token: String, key: String, algorithms: Seq[JwtHmacAlgorithm]): Boolean = ???
+
+  override def decodeRawAll(token: String, key: String, algorithms: Seq[JwtHmacAlgorithm]): Try[(String, String, String)] = ???
 }
