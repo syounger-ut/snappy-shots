111e9853952212648b1ba59f2e906bab1ab9c6c3
Sam Younger <syounger@usertesting.com>
2023-10-11 22:11:04 +0100

    Refactor specs

--- test/com/authService/UnitSpec.scala
+++ test/com/authService/UnitSpec.scala
@@ -2,5 +2,6 @@ package com.authService
 
 import org.scalatest.funspec.AnyFunSpec
 import org.scalamock.scalatest.MockFactory
+import org.scalatest.BeforeAndAfterEach
 
-abstract class UnitSpec extends AnyFunSpec with MockFactory
+abstract class UnitSpec extends AnyFunSpec with MockFactory with BeforeAndAfterEach
--- test/com/authService/controllers/AuthenticationControllerSpec.scala
+++ test/com/authService/controllers/AuthenticationControllerSpec.scala
@@ -7,10 +7,17 @@ import play.api.test._
 import play.api.test.Helpers._
 
 class AuthenticationControllerSpec extends UnitSpec {
-  it("should do something") {
-    val mockAuthService = mock[AuthService]
-    (mockAuthService.createToken _).expects().returning("fake-token")
+  val mockAuthService: AuthService = mock[AuthService]
 
+  def setAuthServiceValues(): Unit = {
+    (mockAuthService.createToken _).expects().returning("fake-token")
+  }
+
+  override def beforeEach(): Unit = {
+    setAuthServiceValues()
+  }
+
+  it("should return an authentication token") {
     val controller = new AuthenticationController(Helpers.stubControllerComponents(), mockAuthService)
     val response = controller.login().apply(FakeRequest())
     val bodyText: String = contentAsString(response)
