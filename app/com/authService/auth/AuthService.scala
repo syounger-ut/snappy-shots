ec38f9672f4e6a6d47a6dc64f58fb7d7af856eca
Sam Younger <syounger@usertesting.com>
2024-01-06 17:38:00 +0000

    Jwt Claim created using JwtClaim class and expiration

--- app/com/authService/auth/AuthService.scala
+++ app/com/authService/auth/AuthService.scala
@@ -1,6 +1,6 @@
 package com.authService.auth
 
-import pdi.jwt.{Jwt, JwtAlgorithm}
+import pdi.jwt.{Jwt, JwtAlgorithm, JwtClaim}
 
 import java.time.Clock
 import scala.util.{Failure, Try}
@@ -8,9 +8,11 @@ import scala.util.{Failure, Try}
 // $COVERAGE-OFF$
 class AuthService {
   implicit val clock: Clock = Clock.systemUTC
+  private val ONE_DAY_SECONDS = 60 * 60 * 24
 
   def createToken(userId: Long): String = {
-    Jwt.encode(s"""{"user":${userId}""", "secretKey", JwtAlgorithm.HS256)
+    val claim = JwtClaim(s"""{"user_id":${userId}""").expiresIn(ONE_DAY_SECONDS)
+    Jwt.encode(claim, "secretKey", JwtAlgorithm.HS256)
   }
 
   def validateToken(token: String): Try[(String, String, String)] = {
