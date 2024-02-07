fd7196d6669d78dd3b158a08d7df9e995d3b74d7
Sam Younger <sam.younger@gmail.com>
2024-02-07 22:42:05 +0000

    13 - Upload repository (#17)
    * Initial aws S3 setup
    
    * StorageAdapter
    
    * An adapter to interact with the AWS S3
    
    * StorageRepository#createBucket
    
    * Business logic to create a bucket
    
    * StorageAdapter move private ClientBuilder to bottom of class
    
    * StorageAdapter#uploadObject
    
    * StorageAdapter#objectExists
    
    * StorageRepository#uploadObject
    
    * Make StorageRepository injectable'
    
    * Disable csrf on /api/storage
    
    * need to enable csrf on the route later
    
    * StorageController#upload
    
    * StorageRepository#uploadObject returns future
    
    * looking for a nicer way to handle failure
    
    * StorageController#upload specs
    
    * StorageAdapter#deleteObject
    
    * StorageRepository#deleteObject
    
    * StorageController#delete
    
    * Delete S3Access which was mvp obj

---
+++ app/com/authService/repositories/StorageAdapter.scala
@@ -0,0 +1,91 @@
+package com.authService.repositories
+
+import com.amazonaws.auth.{AWSStaticCredentialsProvider, BasicAWSCredentials}
+import com.amazonaws.regions.Regions
+import com.amazonaws.services.s3.{AmazonS3, AmazonS3ClientBuilder}
+import com.amazonaws.services.s3.model.{
+  Bucket,
+  CreateBucketRequest,
+  ObjectMetadata,
+  PutObjectRequest,
+  PutObjectResult
+}
+
+import java.io.File
+
+trait IStorageAdapter {
+  def bucketExists(bucketName: String): Boolean
+
+  def createBucket(bucketName: String): Bucket
+
+  def objectExists(bucketName: String, fileName: String): Boolean
+
+  def uploadObject(
+    bucketName: String,
+    fileName: String,
+    file: File
+  ): PutObjectResult
+
+  def deleteObject(
+    bucketName: String,
+    fileName: String
+  ): Unit
+}
+
+trait IClientBuilder {
+  def build: AmazonS3
+}
+
+// $COVERAGE-OFF$
+class StorageAdapter extends IStorageAdapter {
+  private val AWS_ACCESS_KEY =
+    sys.env.getOrElse("SNAPPY_SHOTS_AWS_ACCESS_KEY", "aws-access-key")
+  private val AWS_SECRET =
+    sys.env.getOrElse("SNAPPY_SHOTS_AWS_SECRET", "aws-secret-key")
+
+  def bucketExists(bucketName: String): Boolean =
+    client.doesBucketExistV2(bucketName)
+
+  def createBucket(bucketName: String): Bucket = {
+    val bucketRequest = new CreateBucketRequest(bucketName)
+    client.createBucket(bucketRequest)
+  }
+
+  def objectExists(bucketName: String, fileName: String): Boolean =
+    client.doesObjectExist(bucketName, fileName)
+
+  def uploadObject(
+    bucketName: String,
+    fileName: String,
+    file: File
+  ): PutObjectResult = {
+    val request = new PutObjectRequest(bucketName, fileName, file)
+    val metadata = new ObjectMetadata()
+    metadata.setContentType("plain/text")
+    metadata.addUserMetadata("title", "someTitle")
+    request.setMetadata(metadata)
+
+    client.putObject(request)
+  }
+
+  def deleteObject(bucketName: String, fileName: String): Unit =
+    client.deleteObject(bucketName, fileName)
+
+  private def client = new ClientBuilder(AmazonS3ClientBuilder.standard()).build
+
+  private class ClientBuilder(awsClientBuilder: AmazonS3ClientBuilder)
+    extends IClientBuilder {
+    def build: AmazonS3 = {
+      val credentials = new BasicAWSCredentials(
+        AWS_ACCESS_KEY,
+        AWS_SECRET
+      )
+
+      awsClientBuilder
+        .withCredentials(new AWSStaticCredentialsProvider(credentials))
+        .withRegion(Regions.EU_WEST_2)
+        .build();
+    }
+  }
+}
+// $COVERAGE-ON$
---
+++ app/com/authService/repositories/StorageRepository.scala
@@ -0,0 +1,51 @@
+package com.authService.repositories
+
+import com.amazonaws.services.s3.model.{Bucket, PutObjectResult}
+import com.google.inject.Inject
+
+import java.io.File
+import scala.concurrent.Future
+import scala.util.Try
+
+class StorageRepository @Inject() (storageAdapter: StorageAdapter) {
+  import scala.concurrent.ExecutionContext.Implicits.global
+
+  /* Create a new bucket
+   * @param bucketName: String
+   * @return Bucket
+   */
+  def createBucket(bucketName: String): Try[Bucket] = {
+    Try(storageAdapter.bucketExists(bucketName)).map {
+      case false => storageAdapter.createBucket(bucketName)
+      case true  => throw new IllegalStateException("Bucket already exists")
+    }
+  }
+
+  /* Upload a file to a bucket
+   * @param bucketName: String
+   * @param file: File
+   * @return PutObjectResult
+   */
+  def uploadObject(
+    bucketName: String,
+    fileName: String,
+    file: File
+  ): Future[Try[PutObjectResult]] = {
+    Future.fromTry(Try(storageAdapter.objectExists(bucketName, fileName))).map {
+      case false => Try(storageAdapter.uploadObject(bucketName, fileName, file))
+      case true  => throw new IllegalStateException("File already exists")
+    }
+  }
+
+  /* Delete a file from a bucket
+   * @param bucketName: String
+   * @param fileName: String
+   * @return Unit
+   */
+  def deleteObject(bucketName: String, fileName: String): Future[Unit] = {
+    Future.fromTry(Try(storageAdapter.objectExists(bucketName, fileName))).map {
+      case true  => storageAdapter.deleteObject(bucketName, fileName)
+      case false => throw new IllegalStateException("File does not exist")
+    }
+  }
+}
