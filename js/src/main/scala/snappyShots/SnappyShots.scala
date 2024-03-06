9287f4b2a62a2e37f7b7448c1edd503a2d69fb22
Sam Younger <syounger@usertesting.com>
2024-03-06 21:56:16 +0000

    Remove old vite assets & html

--- js/src/main/scala/snappyShots/SnappyShots.scala
+++ js/src/main/scala/snappyShots/SnappyShots.scala
@@ -19,10 +19,6 @@ import org.scalajs.dom
 
 import scala.language.postfixOps
 
-// import javascriptLogo from "/javascript.svg"
-@js.native @JSImport("/javascript.svg", JSImport.Default)
-val javascriptLogo: String = js.native
-
 @main
 def SnappyShots(): Unit =
   GlobalStyles.addToDocument()
@@ -36,34 +32,7 @@ object Main:
   def appElement(): Element =
     div(
       h1("Snappy Shots"),
-      LoginForm.appElement(),
-      a(
-        href := "https://vitejs.dev",
-        target := "_blank",
-        img(src := "/vite.svg", className := "logo", alt := "Vite logo")
-      ),
-      a(
-        href := "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
-        target := "_blank",
-        img(
-          src := javascriptLogo,
-          className := "logo vanilla",
-          alt := "JavaScript logo"
-        )
-      ),
-      h1("Hello Laminar!"),
-      counterButton(),
-      p(className := "read-the-docs", "Click on the Vite logo to learn more")
+      LoginForm.appElement()
     )
   end appElement
 end Main
-
-def counterButton(): Element =
-  val counter = Var(0)
-  button(
-    tpe := "button",
-    "count is ",
-    child.text <-- counter,
-    onClick --> { event => counter.update(c => c + 1) }
-  )
-end counterButton
